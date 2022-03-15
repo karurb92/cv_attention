dd1e58cb20e24b41752766afc0406e6e3948efcd
karurb92 <karurb92@gmail.com>
2022-03-15 19:41:05 +0100

    implemented glaucoma dataset

--- data_processing/data_generator_approach_2.py
+++ data_processing/data_generator_approach_2.py
@@ -34,7 +34,7 @@ class DataGeneratorA2:
                     if self.flatten_batch:
                         torch_batch[key] = torch.tensor(value, dtype=torch.float32)
                     else:
-                        torch_batch[key] = torch.cat([x.float().unsqueeze(0) for x in value], dim=0).flatten(3,4).flatten(2,3)
+                        torch_batch[key] = torch.cat([x.float().unsqueeze(0) for x in value_tensor], dim=0).flatten(3,4).flatten(2,3)
                 elif key=='label':
                     torch_batch[key] = torch.tensor(value, dtype=torch.long)
                 else:
